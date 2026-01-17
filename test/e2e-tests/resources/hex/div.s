1867122b05d20d10db951b72de29c3ae27a52750
RayZh <rayzhangshanghai@hotmail.com>
2026-01-18 01:13:34 +0800

    chore: update div.s to remove div comments

--- test/e2e-tests/resources/hex/div.s
+++ test/e2e-tests/resources/hex/div.s
@@ -3,21 +3,21 @@
 .global _start
 
 _start:
-    # # A long divide operation
-    # li a0, 20          # Dividend
-    # li a1, 3           # Divisor
-    # div a2, a0, a1     # Quotient
-    # # A short add operation
-    # li a3, 5
-    # li a4, 10
-    # add a5, a3, a4     # Sum
-    # # Put add result
-    # mv a0, a5
-    # call put
-    # # Put div result
-    # mv a0, a2
-    # call put
-    # # End program
+    # A long divide operation
+    li a0, 20          # Dividend
+    li a1, 3           # Divisor
+    div a2, a0, a1     # Quotient
+    # A short add operation
+    li a3, 5
+    li a4, 10
+    add a5, a3, a4     # Sum
+    # Put add result
+    mv a0, a5
+    call put
+    # Put div result
+    mv a0, a2
+    call put
+    # End program
     li a0, 0
     li t0, 0xFFFFFFFF
     sb a0, 0(t0)
